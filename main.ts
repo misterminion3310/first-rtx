e5aebe72867823f8a5eb204961aa73340e33c523
mister minion3310 <daniillisin41@gmail.com>
2026-02-14 11:11:49 +0300

    Обновить pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,14 @@
-input.onButtonPressed(Button.A, function () {
-    count = randint(1, 6)
-    if (count == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    }
-    if (count == 2) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
-            `)
-    }
-    if (count == 3) {
-        basic.showLeds(`
-            # . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . #
-            `)
-    }
-    if (count == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    }
-    if (count == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    }
-    if (count == 6) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    count = 0
-    if (count == 0) {
-        basic.clearScreen()
-    }
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    count = randint(1, 2)
 })
 let count = 0
-basic.clearScreen()
 count = 0
 basic.forever(function () {
-    if (count == 0) {
-        basic.clearScreen()
+    if (count == 1) {
+        basic.showString("Yes")
+    }
+    if (count == 2) {
+        basic.showString("No")
     }
 })
