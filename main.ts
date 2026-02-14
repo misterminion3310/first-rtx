f93d13a9ab46167193604b8e39e49a50d0ad13b2
mister minion3310 <daniillisin41@gmail.com>
2026-02-14 10:59:34 +0300

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,71 @@
+input.onButtonPressed(Button.A, function () {
+    count = randint(1, 6)
+    if (count == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (count == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (count == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    }
+    if (count == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (count == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (count == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    count = 0
+    if (count == 0) {
+        basic.clearScreen()
+    }
+})
+let count = 0
+basic.clearScreen()
+count = 0
+basic.forever(function () {
+    if (count == 0) {
+        basic.clearScreen()
+    }
+})
